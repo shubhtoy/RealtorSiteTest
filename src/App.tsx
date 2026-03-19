7a089d7d21eaec8681456584a431a76eb11accef
Shubh Mittal <shubhmtl@amazon.com>
2026-03-20 02:56:12 +0530

    feat(App): set basename for BrowserRouter to support deployment paths

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ export default function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <EditableContentProvider>
         <AppContent showPreloader={showPreloader} />
       </EditableContentProvider>
